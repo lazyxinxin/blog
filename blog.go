54863dbedcd50defea1ce017540a11c1a5dfaf3a
burke <silyseven@163.com>
2023-11-28 09:29:52 +0000

    add the debug flag

--- blog.go
+++ blog.go
@@ -11,18 +11,36 @@ import (
 
 var log = logrus.New()
 
-func init() {
-	// 初始化Logrus
+const (
+	OutPut_Console = "console"
+	OutPut_File    = "file"
+)
 
-	log.Out = os.Stdout
-	log.SetFormatter(&logrus.TextFormatter{
-		DisableColors:    false, // 启用颜色输出
-		FullTimestamp:    true,
-		TimestampFormat:  "2006-01-02T15:04:05Z07:00",
-		CallerPrettyfier: callerPrettyfier,
-	})
-	log.SetLevel(logrus.InfoLevel) // 设置日志级别为Info
-	log.SetReportCaller(true)
+type Blog struct {
+	IsDebug      bool
+	Log          logrus.Logger
+	OutPutMethod string
+}
+
+func NewBlog(IsDebug bool, OutPutMethod string) *Blog {
+	blog := Blog{
+		IsDebug:      IsDebug,
+		Log:          *logrus.New(),
+		OutPutMethod: OutPutMethod,
+	}
+	// 判断是否是本地conosle输出
+	if OutPutMethod == "console" {
+		log.Out = os.Stdout
+		log.SetFormatter(&logrus.TextFormatter{
+			DisableColors:    false, // 启用颜色输出
+			FullTimestamp:    true,
+			TimestampFormat:  "2006-01-02T15:04:05Z07:00",
+			CallerPrettyfier: callerPrettyfier,
+		})
+		log.SetLevel(logrus.InfoLevel) // 设置日志级别为Info
+		log.SetReportCaller(true)
+	}
+	return &blog
 }
 
 func callerPrettyfier(f *runtime.Frame) (string, string) {
@@ -31,26 +49,39 @@ func callerPrettyfier(f *runtime.Frame) (string, string) {
 	return filename, line
 }
 
-func Info(args ...interface{}) {
+func (b *Blog) Info(args ...interface{}) {
 	log.Info(args...)
 }
 
-func Infof(format string, args ...interface{}) {
+func (b *Blog) Infof(format string, args ...interface{}) {
 	log.Infof(format, args...)
 }
 
-func Warn(args ...interface{}) {
+func (b *Blog) Warn(args ...interface{}) {
 	log.Warn(args...)
 }
 
-func Warnf(format string, args ...interface{}) {
+func (b *Blog) Warnf(format string, args ...interface{}) {
 	log.Warnf(format, args...)
 }
 
-func Error(args ...interface{}) {
+func (b *Blog) Error(args ...interface{}) {
 	log.Error(args...)
 }
 
-func Errorf(format string, args ...interface{}) {
+func (b *Blog) Errorf(format string, args ...interface{}) {
 	log.Errorf(format, args...)
+}
+
+func (b *Blog) Debug(args ...interface{}) {
+	if b.IsDebug {
+		log.Debug(args...)
+	}
+
+}
+
+func (b *Blog) Debugf(format string, args ...interface{}) {
+	if b.IsDebug {
+		log.Debugf(format, args...)
+	}
 }
